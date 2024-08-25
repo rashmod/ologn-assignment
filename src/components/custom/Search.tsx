921fb8858f232c15ed5f74fc04a692a8dee4fdac
Bhushan Alhat <alhatbhushan@gmail.com>
2024-08-25 20:58:13 +0530

    show message when search does not match

--- src/components/custom/Search.tsx
+++ src/components/custom/Search.tsx
@@ -3,7 +3,12 @@
 import { useState } from "react";
 
 import SearchResults from "@/components/custom/SearchResults";
-import { Command, CommandInput, CommandList } from "@/components/ui/command";
+import {
+  Command,
+  CommandEmpty,
+  CommandInput,
+  CommandList,
+} from "@/components/ui/command";
 
 export default function Search() {
   const [input, setInput] = useState("");
@@ -15,11 +20,19 @@ export default function Search() {
         value={input}
         onValueChange={setInput}
       />
+
       <div className="relative animate-in fade-in-0 zoom-in-95">
         <CommandList className="max-h-[200px]">
-          <div className="absolute top-1.5 z-50 w-full">
-            {input.trim().length === 0 ? null : <SearchResults />}
-          </div>
+          {input.trim().length !== 0 && (
+            <div className="absolute top-1.5 z-50 w-full">
+              <CommandEmpty className="min-w-[8rem] rounded-md border bg-background p-1 shadow-md">
+                <div className="flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none">
+                  No results found.
+                </div>
+              </CommandEmpty>
+              <SearchResults />
+            </div>
+          )}
         </CommandList>
       </div>
     </Command>
--- src/components/custom/SearchResults.tsx
+++ src/components/custom/SearchResults.tsx
@@ -10,15 +10,11 @@ import data from "@/data.json";
 export default function SearchResults() {
   return (
     <CommandGroup className="relative z-50 h-[200px] min-w-[8rem] overflow-hidden rounded-md border bg-background shadow-md">
-      {data.length === 0 ? (
-        <CommandEmpty>No results found.</CommandEmpty>
-      ) : (
-        data.map((result, i) => (
-          <CommandItem key={i}>
-            {result.localityName}, {result.cityName}
-          </CommandItem>
-        ))
-      )}
+      {data.map((result, i) => (
+        <CommandItem key={i}>
+          {result.localityName}, {result.cityName}
+        </CommandItem>
+      ))}
     </CommandGroup>
   );
 }
