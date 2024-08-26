04b2fd0036b16e6f1d27304bbe0bba8cf986e332
Bhushan Alhat <alhatbhushan@gmail.com>
2024-08-26 20:36:33 +0530

    added back button on weather page

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -1,4 +1,6 @@
 import { Suspense } from "react";
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
 
 import Weather from "@/components/custom/Weather";
 import Search from "@/components/custom/Search";
@@ -11,6 +13,12 @@ export default function Page({
   return (
     <main className="container flex min-h-screen flex-col items-center justify-center">
       <section className="flex flex-1 flex-col gap-4 py-10">
+        <Link
+          href="/"
+          className="flex w-min items-center gap-1 rounded-md border px-3 py-1 transition hover:bg-muted"
+        >
+          <ChevronLeft /> Back
+        </Link>
         <Search />
         <Suspense fallback={<div>Loading...</div>}>
           <Weather localityId={searchParams?.locality || ""} />
