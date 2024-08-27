f830d7b8e29b0194b25b88a5cf94cd83c84430e7
Bhushan Alhat <alhatbhushan@gmail.com>
2024-08-27 18:08:02 +0530

    logging cwd

--- src/utils/getLocalityList.ts
+++ src/utils/getLocalityList.ts
@@ -2,6 +2,7 @@ import { readFile } from "fs/promises";
 import { Locality } from "@/utils/getLocality";
 
 export default async function getLocalityList() {
+  console.log(process.cwd());
   const file = await readFile(
     process.cwd() + "/src/data/locality.json",
     "utf8",
