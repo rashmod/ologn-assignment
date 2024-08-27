86ce011184a92f0a1bbd03d740d038ddcb2143f8
Bhushan Alhat <alhatbhushan@gmail.com>
2024-08-27 17:24:42 +0530

    fixed json file path

--- src/utils/getLocalityList.ts
+++ src/utils/getLocalityList.ts
@@ -2,7 +2,10 @@ import { readFile } from "fs/promises";
 import { Locality } from "@/utils/getLocality";
 
 export default async function getLocalityList() {
-  const file = await readFile(process.cwd() + "/data/locality.json", "utf8");
+  const file = await readFile(
+    process.cwd() + "/src/data/locality.json",
+    "utf8",
+  );
   const localityList = JSON.parse(file);
 
   return localityList as Locality[];
