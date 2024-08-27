69a94770210c53d7a906d1caa546481cba9bcb2d
Bhushan Alhat <alhatbhushan@gmail.com>
2024-08-27 17:15:42 +0530

    fixed fetch read json data in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Search from "@/components/custom/Search";
+import getLocalityList from "@/utils/getLocalityList";
 
-export default function Home() {
+export default async function Home() {
+  const localityList = await getLocalityList();
+
   return (
     <main className="container flex min-h-dvh items-center justify-center">
       <section className="grid grid-rows-3 gap-4 py-2 sm:gap-8">
         <h1 className="text-center text-5xl font-bold sm:text-9xl">Woogle</h1>
-        <Search />
+        <Search localityList={localityList} />
       </section>
     </main>
   );
